d05dcaf49956c5369bb2ddd6de7de3f76de7dbb1
Faisal A. Salam <faisaltez@gmail.com>
2022-02-12 23:29:32 +0600

    Class 2

---
+++ class_2/bin/class_2.dart
@@ -0,0 +1,44 @@
+void main() {
+
+  // This is comment single line
+
+  /* This is multiple lines 1
+  This is multiple lines 1 */
+
+
+  // int phoneNum = 01231323213;
+
+  // String fullName = "Faisal";
+
+  // bool isMale = true;
+
+  // List friendLists = [];
+
+  // Map myMap = {};
+
+  String studentName = "Faisal";
+
+  int studentRollNo = 110;
+
+  double studentCgpa = 3.5;
+
+  // print(studentName);
+
+  // print('Name: $studentName Roll No. $studentRollNo CGPA: $studentCgpa');
+  // print('Name: $studentName \nRoll No. $studentRollNo \nCGPA: $studentCgpa');
+
+  int num1 = 20;
+  int num2 = 10;
+
+  int sum = num1 + num2;
+  int sub = num1 - num2;
+  int mul = num1 * num2;
+  double div = num1 / num2;
+
+  print(sum);
+  print(sub);
+  print(mul);
+  print(div);
+
+
+}
